d50b5821cba9b2ba902f8ae8f00cb1bf4aae66b3
mieum12 <qpdlqltb1215@gmail.com>
2024-02-23 00:04:55 +0900

    feat: emotion 적용, 네비게이션 디자인 적용

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,22 +1,37 @@
 import Link from "next/link";
+import styled from "@emotion/styled";
 
-function MainNavigation() {
+export default function MainNavigation() {
 
   return (
     <header>
-      <Link href='/'>MAEUM</Link>
-      <nav>
+      <Nav>
+        <Link href='/'>MAEUM</Link>
         <ul>
           <li>
-            <Link href='/ai'>AI analysis</Link>
+            <Link href='/ai'>AI Analysis</Link>
           </li>
           <li>
             <Link href='/posts'>All Posts</Link>
           </li>
+          <li>
+            <Link href='/'>My Page</Link>
+          </li>
         </ul>
-      </nav>
+      </Nav>
     </header>
   );
 }
 
-export default MainNavigation;
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  ul {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: baseline;
+  }
+`
