757760eaaa3b4d985c519229a23abedd0ca02a1d
mieum12 <qpdlqltb1215@gmail.com>
2024-02-29 02:34:33 +0900

    feat: 회원가입 요청

--- components/auth/AuthForm.js
+++ components/auth/AuthForm.js
@@ -1,13 +1,12 @@
-import {useRouter} from "next/router";
 import {useRef, useState} from "react";
 import styled from "@emotion/styled";
+import CreateUser from "@/components/auth/CreateUser";
 
 export default function AuthForm() {
   const emailInputRef = useRef()
   const passwordInputRef = useRef()
 
   const [isLogin, setIsLogin] = useState(true);
-  const router =useRouter()
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -16,9 +15,25 @@ export default function AuthForm() {
   async function submitHandler(e) {
     e.preventDefault();
 
-    // ref가정 있으니 데이터를 가져올 수 있다
+    // ref가 있으니 데이터를 가져올 수 있다
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
+    console.log('이메일',enteredEmail,'비번',enteredPassword)
+
+    // 0. 클라이언트 사이드 유효성 검사 여기에 추가해도 됨
+
+    // 1. 로그인 모드일때 폼 제출 = 로그인 시킨다
+    if (isLogin) {
+
+    } else {
+      // 2. 회원가입 모드일 때 = 회원가입 로직
+      try {
+        const result = await CreateUser(enteredEmail, enteredPassword)
+        console.log('사용자 생성 완료!',result)
+      } catch (e) {
+        console.log(e)
+      }
+    }
 
   }
 
