d67314d52d2a320df4f39bcd8287006e36e33c8f
mieum12 <qpdlqltb1215@gmail.com>
2024-02-29 17:49:51 +0900

    feat: session provider로 감싸기

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout/Layout";
+import {SessionProvider} from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    // 내부 코드를 최적화 해서 useSession이 보내는
+    // 추가적인 HTTP 요청 (세션 유효성 검사 요청)을 건너뛰게 함
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+
+  )
 }
